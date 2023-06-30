3022aeec8ebf57d0530d99732da6cfeb8d462f40
yuni-0928 <yuniyuni0928@gmail.com>
2023-07-01 05:00:14 +0900

    生成されるcreepを強化

--- src/spawn.harvester.js
+++ src/spawn.harvester.js
@@ -2,7 +2,7 @@ const spawnHarvester = {
   run: function (spawn) {
     let newName = 'Harvester' + Game.time;
     console.log('Spawning new harvester: ' + newName);
-    spawn.spawnCreep([WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE], newName, {
+    spawn.spawnCreep([WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE], newName, {
       memory: { role: 'harvester' },
     });
   }
--- src/spawn.upgrader.js
+++ src/spawn.upgrader.js
@@ -2,7 +2,7 @@ const spawnUpgrader = {
   run: function (spawn) {
     const newName = 'Upgrader' + Game.time;
     console.log('Spawning new harvester: ' + newName);
-    spawn.spawnCreep([WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE], newName, {
+    spawn.spawnCreep([WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE], newName, {
       memory: { role: 'upgrader' },
     });
   }
