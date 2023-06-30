36c4b5d39e67c8a8567d42cdf205e2534083bb04
yuni-0928 <yuniyuni0928@gmail.com>
2023-06-30 20:57:50 +0900

    既に書いていたコードを置く

---
+++ managementCreepCount.js
@@ -0,0 +1,15 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('managementCreepCount');
+ * mod.thing == 'a thing'; // true
+ */
+
+const managementCreepCount = {
+  harvester: 5,
+  upgrader: 5,
+  builder: 5,
+};
+module.exports = managementCreepCount;
---
+++ role.upgrader.js
@@ -0,0 +1,50 @@
+/*
+ * Module code goes here. Use 'module.exports' to export things:
+ * module.exports.thing = 'a thing';
+ *
+ * You can import it from another modules like this:
+ * var mod = require('role.upgrader');
+ * mod.thing == 'a thing'; // true
+ */
+
+var getRandomInt = require('getRandomInt');
+
+var roleUpgrader = {
+
+  /** @param {Creep} creep **/
+  run: function (creep) {
+
+    if (creep.memory.upgrading && creep.store[RESOURCE_ENERGY] == 0) {
+      creep.memory.upgrading = false;
+
+      var sources = creep.room.find(FIND_SOURCES);
+      var randTargetId = sources[getRandomInt(sources.length)].id
+      creep.memory.harvestTargetId = randTargetId;
+
+      creep.say('🔄 harvest');
+    }
+    if (!creep.memory.upgrading && creep.store.getFreeCapacity() == 0) {
+      creep.memory.upgrading = true;
+      creep.say('⚡ upgrade');
+    }
+
+    if (creep.memory.upgrading) {
+      if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(creep.room.controller, { visualizePathStyle: { stroke: '#ffffff' } });
+      }
+    }
+    else {
+      if (creep.memory.harvestTargetId) {
+        var sources = []
+        sources.push(Game.getObjectById(creep.memory.harvestTargetId));
+      } else {
+        var sources = creep.room.find(FIND_SOURCES);
+      }
+      if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0], { visualizePathStyle: { stroke: '#ffaa00' } });
+      }
+    }
+  }
+};
+
+module.exports = roleUpgrader;
