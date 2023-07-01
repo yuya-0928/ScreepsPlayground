009a8b14ce10f73096b34ca655280fd8e970920f
yuni-0928 <yuniyuni0928@gmail.com>
2023-07-01 18:52:01 +0900

    creepのコストを下げる

--- src/spawn.repaierer.js
+++ src/spawn.repaierer.js
@@ -2,7 +2,7 @@ const spawnRepaierer = {
   run: function (spawn) {
     const newName = 'Repaierer' + Game.time;
     console.log('Spawning new repaierer: ' + newName);
-    spawn.spawnCreep([WORK, WORK, WORK, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE, MOVE], newName, {
+    spawn.spawnCreep([WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE], newName, {
       memory: { role: 'repaierer' },
     });
   }
