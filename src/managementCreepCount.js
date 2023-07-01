0ccf620005dc5b22a3fda4383bfe33cf5caf4112
yuni-0928 <yuniyuni0928@gmail.com>
2023-07-01 15:18:32 +0900

    新しい役割のBotを管理対象に追加する

--- src/managementCreepCount.js
+++ src/managementCreepCount.js
@@ -2,5 +2,9 @@ const managementCreepCount = {
   harvester: 5,
   upgrader: 5,
   builder: 5,
+  repaierer: 5,
+  attacker: 5,
+  healer: 5,
+  rangedAttacker: 5,
 };
 module.exports = managementCreepCount;
