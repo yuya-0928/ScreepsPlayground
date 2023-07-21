b47ce7f681b068211e15c20544a2622b947031cd
yuya-0928 <y.hirako0928@gmail.com>
2023-07-22 01:48:12 +0900

    spawn.upgrader.jsをtypescript化する

---
+++ typeScriptScreepsProject/src/spawn.upgrader.ts
@@ -0,0 +1,15 @@
+export const spawnUpgrader = {
+  run: function (spawn: StructureSpawn) {
+    const newName = "Upgrader" + Game.time;
+    console.log("Spawning new harvester: " + newName);
+    spawn.spawnCreep(
+      [WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE],
+      newName,
+      {
+        memory: { role: "upgrader" },
+      }
+    );
+  },
+};
+
+module.exports = spawnUpgrader;
