9ec3a1961736231faf5c0dbbb8d3775473e99602
yuya-0928 <y.hirako0928@gmail.com>
2023-07-22 01:36:22 +0900

    spawn.harvester.jsをtypescript化する

---
+++ typeScriptScreepsProject/src/spawn.harvester.ts
@@ -0,0 +1,15 @@
+const spawnHarvester = {
+  run: function (spawn: StructureSpawn) {
+    let newName = "Harvester" + Game.time;
+    console.log("Spawning new harvester: " + newName);
+    spawn.spawnCreep(
+      [WORK, WORK, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE],
+      newName,
+      {
+        memory: { role: "harvester" },
+      }
+    );
+  },
+};
+
+module.exports = spawnHarvester;
