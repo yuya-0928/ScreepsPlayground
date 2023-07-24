fe2d348f5e6a43bddcbefc85b2a9c7221d73e1ae
yuya-0928 <y.hirako0928@gmail.com>
2023-07-24 22:46:28 +0900

    action.moveをリファクタリング

--- typeScriptScreepsProject/src/modules/role/role.builder.ts
+++ typeScriptScreepsProject/src/modules/role/role.builder.ts
@@ -2,44 +2,55 @@ import { findTarget } from "../findTarget";
 import { actionHarvest } from "../action/action.harvest";
 import { memoryManager } from "../memoryManager";
 import { CreepMemory } from "../../main";
+import { actionMove } from "../action/action.move";
+
+const isBuilding = (creep: Creep) => {
+  return (creep.memory as CreepMemory).building;
+};
+
+const isStoreEmpty = (creep: Creep) => {
+  return creep.store[RESOURCE_ENERGY] == 0;
+};
+
+const isStoreFull = (creep: Creep) => {
+  return creep.store.getFreeCapacity() == 0;
+};
 
 export const roleBuilder = {
-  /** @param {Creep} creep **/
-  run: function (creep: Creep) {
-    if (
-      (creep.memory as CreepMemory).building &&
-      creep.store[RESOURCE_ENERGY] == 0
-    ) {
-      memoryManager.refreshMemory(creep);
-      (creep.memory as CreepMemory).building = false;
-      const randTargetId = findTarget.randomSourcesFind(creep);
-      (creep.memory as CreepMemory).harvestTargetId = randTargetId;
-      (creep.memory as CreepMemory).roleAs = "builder";
-      creep.say("🔄 harvest");
-    }
-    if (
-      !(creep.memory as CreepMemory).building &&
-      creep.store.getFreeCapacity() == 0
-    ) {
-      memoryManager.refreshMemory(creep);
-      (creep.memory as CreepMemory).building = true;
-      (creep.memory as CreepMemory).roleAs = "builder";
-      creep.say("🚧 build");
-    }
+  run: (creep: Creep) => {
+    (creep.memory as CreepMemory).roleAs = "builder";
 
-    if ((creep.memory as CreepMemory).building) {
-      let targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-      if (targets.length) {
-        if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
-          // TODO: Creepの動作状態をMemoryに保存
-          creep.moveTo(targets[0], {
-            visualizePathStyle: { stroke: "#ffffff" },
-          });
+    switch (isBuilding(creep)) {
+      case true:
+        if (isStoreEmpty(creep)) {
+          memoryManager.refreshMemory(creep);
+          const randTargetId = findTarget.randomSourcesFind(creep);
+          (creep.memory as CreepMemory).building = false;
+          (creep.memory as CreepMemory).harvestTargetId = randTargetId;
+          creep.say("🔄 harvest");
+          break;
         }
-      }
-    } else {
-      // TODO: Creepの動作状態をMemoryに保存
-      actionHarvest.run(creep);
+
+        let targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+        if (targets.length) {
+          if (creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+            // TODO: Creepの動作状態をMemoryに保存
+            actionMove(creep, targets[0]);
+          }
+        }
+        break;
+
+      case false:
+        if (isStoreFull(creep)) {
+          memoryManager.refreshMemory(creep);
+          (creep.memory as CreepMemory).building = true;
+          creep.say("🚧 build");
+          break;
+        }
+
+        // TODO: Creepの動作状態をMemoryに保存
+        actionHarvest.run(creep);
+        break;
     }
   },
 };
