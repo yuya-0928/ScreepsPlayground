928328b830275e8f04f5e6dc67c0195d504b7b92
yuya-0928 <y.hirako0928@gmail.com>
2023-07-28 23:47:58 +0900

    名前を変更

--- typeScriptScreepsProject/src/modules/find/findTowers.ts
+++ typeScriptScreepsProject/src/modules/find/findTowers.ts
@@ -1,4 +1,4 @@
-export const findSpawners = (creep: Creep) => {
+export const findTowers = (creep: Creep) => {
   return creep.room.find(FIND_STRUCTURES, {
     filter: (structure) => {
       return structure.structureType == STRUCTURE_TOWER;
